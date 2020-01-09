d7b64148d32d505de443bf255efee40e31eccb9d
JasonBourne <1219709884@qq.com>
2020-01-09 21:09:36 +0800

    最新

--- GXLCodeRepository/Classes/UI控件/alert弹框/AlertViewTEST.m
+++ GXLCodeRepository/Classes/UI控件/alert弹框/AlertViewTEST.m
@@ -11,10 +11,12 @@
 @interface AlertViewTEST ()<UITableViewDelegate, UITableViewDataSource>
 /** 表单 */
 @property (nonatomic, strong) UITableView *tableView;
+/** 数据 */
+@property (nonatomic, strong) NSArray *dataSource;
 @end
 
 @implementation AlertViewTEST
-
+#pragma mark - 生命周期
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
@@ -24,6 +26,8 @@
     [self.view addSubview:self.tableView];
 }
 
+
+#pragma mark - 创建UI
 - (UITableView *)tableView
 {
     if (_tableView == nil) {
@@ -35,10 +39,19 @@
     return _tableView;
 }
 
+#pragma mark - 初始化数据
+- (NSArray *)dataSource
+{
+    if (_dataSource == nil) {
+        _dataSource = @[@""];
+    }
+    return _dataSource;
+}
+
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 3;
+    return 5;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -65,6 +78,30 @@
         [CZProgressHUD showProgressHUDWithText:@"弹框样式2"];
     } else if (indexPath.row == 2) {
         [CZProgressHUD showOrangeProgressHUDWithText:@"弹框样式3"];
+    } else if (indexPath.row == 3) {
+        UIAlertController *alertView = [UIAlertController alertControllerWithTitle:@"提示" message:@"弹框样式4" preferredStyle:UIAlertControllerStyleAlert];
+        [alertView addAction:[UIAlertAction actionWithTitle:@"默认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+
+        }]];
+        [alertView addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+
+        }]];
+        [alertView addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+
+        }]];
+        [self presentViewController:alertView animated:YES completion:nil];
+    } else if (indexPath.row == 4) {
+        UIAlertController *alertView = [UIAlertController alertControllerWithTitle:@"提示" message:@"弹框样式5" preferredStyle:UIAlertControllerStyleActionSheet];
+        [alertView addAction:[UIAlertAction actionWithTitle:@"默认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+
+        }]];
+        [alertView addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+
+        }]];
+        [alertView addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+
+        }]];
+        [self presentViewController:alertView animated:YES completion:nil];
     }
     [CZProgressHUD hideAfterDelay:1.5];
 }
